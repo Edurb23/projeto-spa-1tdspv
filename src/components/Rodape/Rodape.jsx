502ab4411f684dbe0d052eeb7a1b76c679f832f8
Edurb23 <eddurb64@gmail.com>
2023-10-04 12:25:32 -0300

    first commit

---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,33 @@
+import "./Rodape.scss";
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter, faReddit} from '@fortawesome/free-brands-svg-icons'
+//import { faGithub, faTwitter, faReddit } from '@fortawesome/free-brands-svg-icons';
+import "./Rodape.scss";
+
+
+export default function Rodape() {
+  return (
+    <footer className="footer">
+      <div>
+        <ul className="list-items">
+          <li className="item">
+            <a href="https://github.com">
+              <FontAwesomeIcon icon={faGithub} /> GitHub
+            </a>
+          </li>
+          <li className="item">
+            <a href="https://twitter.com">
+              <FontAwesomeIcon icon={faTwitter} /> Twitter
+            </a>
+          </li>
+          <li className="item">
+            <a href="https://www.reddit.com">
+              <FontAwesomeIcon icon={faReddit} /> Reddit
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
